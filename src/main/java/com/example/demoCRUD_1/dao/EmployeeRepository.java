9766e5da990da067602c83a09832e12a641266f4
nazarchykur <nazarchykur@gmail.com>
2020-01-06 17:11:06 +0200

    4.3. fix default - Spring Boot - Spring Data REST - Configuration, Pagination and Sorting

--- src/main/java/com/example/demoCRUD_1/dao/EmployeeRepository.java
+++ src/main/java/com/example/demoCRUD_1/dao/EmployeeRepository.java
@@ -4,6 +4,6 @@ import com.example.demoCRUD_1.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "members")
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
