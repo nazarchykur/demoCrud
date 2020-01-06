7ba06f2c10ca1a429abd5baa56264839db92dc49
nazarchykur <nazarchykur@gmail.com>
2020-01-06 15:58:57 +0200

    4.1. Spring Boot - Spring Data REST - Creating a REST API

--- src/main/java/com/example/demoCRUD_1/DemoCrudApplication.java
+++ src/main/java/com/example/demoCRUD_1/DemoCrudApplication.java
@@ -6,6 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoCrudApplication {
 
+    // no need service and controller
+	// only add dependency <spring-boot-starter-data-rest> to our POM.XML
+	// and we have all functionality for free
+	// all crud operations are works fine
+
+	// all we need to remember that we have ENTITY -> Employee
+	// to our URI we add name of this entity which starts w/ small caps
+	// and add 's' to end =>
+	// fot our CRUD we use /employees
 	public static void main(String[] args) {
 		SpringApplication.run(DemoCrudApplication.class, args);
 	}
