93c311065ced4b26fab57979276c8e11f876f5e9
nazarchykur <nazarchykur@gmail.com>
2020-01-06 16:37:26 +0200

    4.2. Spring Boot - Spring Data REST - Configuration, Pagination and Sorting

--- src/main/java/com/example/demoCRUD_1/DemoCrudApplication.java
+++ src/main/java/com/example/demoCRUD_1/DemoCrudApplication.java
@@ -15,6 +15,19 @@ public class DemoCrudApplication {
 	// to our URI we add name of this entity which starts w/ small caps
 	// and add 's' to end =>
 	// fot our CRUD we use /employees
+
+	// if we need specify the name we need add @RepositoryRestResource(path="nameRepo")
+	// at our interface repository which one extends JpaRepository
+
+	// in application.properties file we can define our Spring Data REST props
+	// #spring.data.rest.base-path=/our-api     for  base-path
+	// #spring.data.rest.default-page-size=10    for pagination => size el per page
+	// and other...
+
+	// for sorting in URI use ?sort=lastName,desc
+	// sorting by lastName by descending
+	// we can sort by any name of field our ENTITY
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoCrudApplication.class, args);
 	}
--- src/main/java/com/example/demoCRUD_1/dao/EmployeeRepository.java
+++ src/main/java/com/example/demoCRUD_1/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.example.demoCRUD_1.dao;
 
 import com.example.demoCRUD_1.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
